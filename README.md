3c4887cad90ff2aa27fa71239a6ad05e38a1e1fd
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-09-09 12:51:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Assignment 0
+
+You should edit the "answer.py" file and answer the questions.
+
+Please don't try to rename and edit any other files.
+
+## Make sure your code is bug-free and ready to run
+Test your code before submitting.
+## Good luck!
---
+++ answer.py
@@ -0,0 +1,17 @@
+def hello_world():
+    # Write a doc string below. Doc strings can be used to provide users with a
+    # description of what the function does.
+    """
+
+    """
+
+    # Write a comment; comments are very useful for other programmers to understand.
+
+    # Write a Python statement to print Hello, World!
+
+def add(a,b):
+
+    # Write simple code to add variables a and b
+    output =
+
+    return output
